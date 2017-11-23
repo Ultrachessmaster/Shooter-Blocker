18a756fd5d0a1c9601861457b8a05abb3e3c9085
David Van Horn <Ultrachessmaster@users.noreply.github.com>
2017-11-22 22:23:43 -0700

    Main and Player Sprite

--- Main.py
+++ Main.py
@@ -1,12 +1,28 @@
 import pygame as pg
+from pygame.locals import *
 
 pg.init()
 
-size = 800, 300
+size = 1000, 600
 screen = pg.display.set_mode(size)
 clock = pg.time.Clock()
+ballimage = pg.image.load("player.png")
+sprite = pg.sprite.Sprite()
+sprite.image = ballimage
+sprite.rect = ballimage.get_rect()
 spritelist = pg.sprite.RenderPlain()
+spritelist.add(sprite)
+running = True
 
-while True:
+while running:
     clock.tick(60)
+
+    for event in pg.event.get():
+        if event.type == QUIT:
+            running = False
+
     spritelist.update()
+    sprite.rect = sprite.rect.move([1,1])
+    screen.fill((0,0,0))
+    spritelist.draw(screen)
+    pg.display.flip()
